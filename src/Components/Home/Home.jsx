ea1f2ddd26dfd6d09d6894d1e9f71b23a7a12f11
andreitol <tolstoi.a@gmail.com>
2020-05-18 15:53:27 +0300

    feat:Product implement hoock has changing title name

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,7 +5,22 @@ import { Layout } from 'antd';
 import styles from './Home.module.css';
 const { Header, Footer, Sider, Content } = Layout;
 
+
+const data = [
+  {
+    id: 'pb01',
+    name: 'Пицца от Шеф-повара',
+    description: 'Сыр моцарелла, помидоры, салями, бекон, сыр пармезан, зелень, ветчина',
+    price: 638,
+    image: './../../data/img/pizza-burger/pizza-dacha.jpg',
+    // image: 'img/pizza-burger/pizza-dacha.jpg',
+  },
+];
+
 export const Home = () => {
+
+  const clickProduct = () => {};
+
   return (
     <Layout>
       <Header>Header</Header>
@@ -13,7 +28,8 @@ export const Home = () => {
         <Sider>Sider</Sider>
         <Content>
           <h2> Content</h2>
-          <Product />
+
+          <Product data={data} />
         </Content>
       </Layout>
       <Footer>Footer</Footer>
--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,22 +1,31 @@
-import React from 'react';
-import image from '../../data/data-img/pizza-burger/pizza-caesar.jpg';
+import React, {useState} from 'react';
+import image from '../../data/img/pizza-burger/pizza-dacha.jpg';
 
+import { Card, Button } from 'antd';
+const { Meta } = Card;
 
-export const Product = () => {
+export const Product = (props) => {
+  const { id, name, description, price, imageA } = props.data[0];
+
+  // let title = name
+  const [title, changeTitle] = useState(name)
+
+  const clickProductCard = () => {
+    // title = 'Clicked';
+    changeTitle('Clicked');
+    console.log('click Card')
+  }
+
   return (
-    <article>
-      <img src={image} alt={'caesar'} className='card-image' />
-      <div className='card-text'>
-        <div className='card-heading'>
-          <h3 className='card-title'>{'Caesar'}</h3>
-          <span className='card-tag tag'>{'timeOfDelivery'} мин</span>
-        </div>
-        <div className='card-info'>
-          <div className='rating'>{'stars'}</div>
-          <div className='price'>От {'price'} ₽</div>
-          <div className='category'>{'kitchen'}</div>
-        </div>
+    <Card key={id} hoverable style={{ width: 280 }} cover={<img alt={name} src={image} />} onClick={clickProductCard}>
+      <Meta title={title} description={description} />
+      <div>
+        <Button>
+          <span>В корзину</span>
+          <span className='button-cart-svg'></span>
+        </Button>
+        <strong>{price} ₽</strong>
       </div>
-    </article>
+    </Card>
   );
-}
+};
