89f81dac09efbf845044e70871b7c25bb4dd9b56
andreitol <tolstoi.a@gmail.com>
2020-05-21 11:14:58 +0300

    feat:Basket add function that calculate cost of current product in basket

--- src/Components/Home/Basket/Basket.jsx
+++ src/Components/Home/Basket/Basket.jsx
@@ -1,41 +1,84 @@
 import React from 'react';
 import { BasketItem } from './BasketItem/BasketItem';
 
-const inBasket = [
-  {
-    id: 'pb01',
-    name: 'Пицца от Шеф-повара',
-    description: 'Сыр моцарелла, помидоры, салями, бекон, сыр пармезан, зелень, ветчина',
-    price: 638,
-    image: 'img/pizza-burger/pizza-dacha.jpg',
-  },
-  {
-    id: 'pb02',
-    name: 'Пицца Мясное ассорти',
-    description: 'Сыр моцарелла, фирменный соус, помидоры, куриная грудка, бекон, свинина, говядина, зелень',
-    price: 693,
-    image: 'img/pizza-burger/pizza-meat.jpg',
-  },
-  {
-    id: 'pp01',
-    name: 'Пицца Везувий',
-    description: 'Соус томатный, сыр «Моцарелла», ветчина, пепперони, перец «Халапенье», соус «Тобаско», томаты.',
-    price: 545,
-    image: 'img/pizza-plus/pizza-vesuvius.jpg',
-  },
-  {
-    id: 'pp02',
-    name: 'Пицца Девичник',
-    description:
-      'Соус томатный, постное тесто, нежирный сыр, кукуруза, лук, маслины, грибы, помидоры, болгарский перец.',
-    price: 450,
-    image: 'img/pizza-plus/pizza-girls.jpg',
-  },
-];
+const inBasket = {
+  inBasketState: [
+    { id: 'pp02', quantity: 1 },
+    { id: 'pp01', quantity: 1 },
+    { id: 'pb02', quantity: 1 },
+    { id: 'pb01', quantity: 3 },
+  ],
+
+  inBasketProducts: [
+    {
+      id: 'pb01',
+      name: 'Пицца от Шеф-повара',
+      description: 'Сыр моцарелла, помидоры, салями, бекон, сыр пармезан, зелень, ветчина',
+      price: 638,
+      image: 'img/pizza-burger/pizza-dacha.jpg',
+    },
+    {
+      id: 'pb02',
+      name: 'Пицца Мясное ассорти',
+      description: 'Сыр моцарелла, фирменный соус, помидоры, куриная грудка, бекон, свинина, говядина, зелень',
+      price: 693,
+      image: 'img/pizza-burger/pizza-meat.jpg',
+    },
+    {
+      id: 'pp01',
+      name: 'Пицца Везувий',
+      description: 'Соус томатный, сыр «Моцарелла», ветчина, пепперони, перец «Халапенье», соус «Тобаско», томаты.',
+      price: 545,
+      image: 'img/pizza-plus/pizza-vesuvius.jpg',
+    },
+    {
+      id: 'pp02',
+      name: 'Пицца Девичник',
+      description:
+        'Соус томатный, постное тесто, нежирный сыр, кукуруза, лук, маслины, грибы, помидоры, болгарский перец.',
+      price: 450,
+      image: 'img/pizza-plus/pizza-girls.jpg',
+    },
+  ],
+};
 
 export const Basket = () => {
-  const renderBasket = () => inBasket
-    .map((product) => <BasketItem key={product.id} item={product} />);
+  const updateData = (price, count) => {
+    let a = price * count;
+  };
 
-  return <section className='container mx-auto p-2 bg-gray-200'>{renderBasket()}</section>;
+  /**
+   * Функция по id товара находит его стоимость и количество в корзине, и вычисляет общую стоимость конкретного товара
+   * The function by id of the product finds its value and quantity in the basket, and calculates the total cost of a particular product
+   * @param {number} id  -  товара
+   */
+  const totalPositionCost = (id) => {
+    const indxQuantity = inBasket.inBasketState.findIndex((el) => el.id === id);
+    const indxPrice = inBasket.inBasketProducts.findIndex((el) => el.id === id);
+
+    const positionQuantity = inBasket.inBasketState[indxQuantity].quantity;
+    const positionPrice = inBasket.inBasketProducts[indxPrice].price;
+
+    return positionQuantity * positionPrice;
+  };
+  console.log(totalPositionCost('pb01'));
+
+  const renderBasket = () =>
+    inBasket.inBasketProducts.map((product) => {
+      return (
+        <BasketItem key={product.id} item={product} updateData={updateData} inBasketState={inBasket.inBasketState} />
+      );
+    });
+
+  return (
+    <div className='container flex'>
+      <section className='container w-2/3 mx-auto p-2 bg-gray-200'>{renderBasket()}</section>
+      <section className='container w-1/3 p-2'>
+        <div className='container w-full h-full border border-gray-400 p-4'>
+          <p className='font-medium mb-4'>Всего товаров: {inBasket.inBasketState.length}</p>
+          <p className='font-bold'>Товары на сумму: </p>
+        </div>
+      </section>
+    </div>
+  );
 };
--- src/Components/Home/Basket/BasketItem/BasketItem.jsx
+++ src/Components/Home/Basket/BasketItem/BasketItem.jsx
@@ -1,41 +1,59 @@
 import React, { useState } from 'react';
 
 /**
- * Товар в корзине и его количество
+ * Товар в корзине, его цена и количество
  */
 export const BasketItem = (props) => {
   const { name, price, image } = props.item;
-  const [count, setCount] = useState(0);
+
+  const count = 1
+
+  // изменяется количество товаров
+  const [currentCount, setCount] = useState(count);
 
   const removeCount = () => {
-    if (count) {
-      setCount(count - 1);
+    if (currentCount) {
+      setCount(currentCount - 1);
     }
   };
   const addCount = () => {
-    setCount(count + 1);
+    setCount(currentCount + 1);
   };
-  // debugger
+
+  // Изменяется сумма за одну позицию
+  const [sumItem, setSum] = useState(price);
+
+  const allPriceOfPosition = () => {
+    setSum(price * currentCount);
+  };
 
   return (
-    <div className='flex justify-between items-center bg-gray-400 px-4 py-1 mb-1'>
+    <div className='flex justify-between items-center bg-gray-400 px-4 py-0 mb-1'>
       <div className='w-2/12'>
         <img className='bg-gray-600 max-w-none w-16 h-16' src={image} alt={name} />
       </div>
       <div className='w-4/12 mr-8 my-4'>{name}</div>
-      <strong className='w-4/12 mr-8 my-4'>{price} ₽</strong>
-      <div className='w-2/12 my-4'>
-        <button
-          className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'
-          onClick={removeCount}>
-          -
-        </button>
-        <span className='px-6'>{count}</span>
+      <strong className='w-1/12 mr-8 my-4'>{price} ₽</strong>
+      <strong className='w-1/12 mr-8 my-4'>{sumItem} ₽</strong>
+      <div className='flex items-center justify-end  w-4/12 my-4'>
         <button
           className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'
           onClick={() => {
-            console.log('click')
-            addCount()
+            removeCount();
+            allPriceOfPosition();
+
+            props.updateData(price, currentCount);
+          }}>
+          -
+        </button>
+        <span className='px-6'>{currentCount}</span>
+        <button
+          className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'
+          onClick={() => {
+            addCount();
+            allPriceOfPosition();
+            console.log(currentCount);
+            props.updateData(price, currentCount);
           }}>
           +
         </button>
