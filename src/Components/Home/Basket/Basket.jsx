66e84246018f04ef8d01440f2087a83f52d5ae29
andreitol <tolstoi.a@gmail.com>
2020-05-23 22:33:17 +0300

    fix:Basket minor change

--- src/Components/Home/Basket/Basket.jsx
+++ src/Components/Home/Basket/Basket.jsx
@@ -149,7 +149,6 @@ export const Basket = () => {
           increaseQuantity={increaseQuantity}
           decreaseQuantity={decreaseQuantity}
           changeQuantity={changeQuantity}
-          productSum={productSum}
         />
       );
     });
